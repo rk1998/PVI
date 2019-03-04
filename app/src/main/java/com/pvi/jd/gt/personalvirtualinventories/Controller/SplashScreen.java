ab21d8bc31fd1ed6dd0e550c30e96bc59c26a5e6
Dixie <dixierich@lawn-143-215-122-149.lawn.gatech.edu>
2019-03-04 11:02:00 -0500

    created splash screen and opening screen

---
+++ app/src/main/java/com/pvi/jd/gt/personalvirtualinventories/Controller/SplashScreen.java
@@ -0,0 +1,15 @@
+package com.pvi.jd.gt.personalvirtualinventories.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pvi.jd.gt.personalvirtualinventories.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
