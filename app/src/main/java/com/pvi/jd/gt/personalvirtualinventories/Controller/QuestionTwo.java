6dc7269a33059ed895c8e3dcde6179cceec81b77
Dixie <dixierich@MacBook-Pro-43.local>
2019-03-05 21:46:50 -0500

    updated question one and two

--- app/src/main/java/com/pvi/jd/gt/personalvirtualinventories/Controller/QuestionTwo.java
+++ app/src/main/java/com/pvi/jd/gt/personalvirtualinventories/Controller/QuestionTwo.java
@@ -1,9 +1,12 @@
 package com.pvi.jd.gt.personalvirtualinventories.Controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import com.pvi.jd.gt.personalvirtualinventories.R;
 
@@ -18,6 +21,16 @@ public class QuestionTwo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_two);
+
+        Button next = (Button) findViewById(R.id.next_button_q2);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextIntent = new Intent(QuestionTwo.this,
+                        QuestionThree.class);
+                startActivity(nextIntent);
+            }
+        });
 
     }
 }
