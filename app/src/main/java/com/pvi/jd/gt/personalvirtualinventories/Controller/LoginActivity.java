7c1c3b394f51be1c2e2abf9579a33a197e843921
Dixie <dixierich@MacBook-Pro-43.local>
2019-03-05 21:47:55 -0500

    connected all login/registration/opening screens

--- app/src/main/java/com/pvi/jd/gt/personalvirtualinventories/Controller/LoginActivity.java
+++ app/src/main/java/com/pvi/jd/gt/personalvirtualinventories/Controller/LoginActivity.java
@@ -3,6 +3,7 @@ package com.pvi.jd.gt.personalvirtualinventories.Controller;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -94,6 +95,16 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         //mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+
+        mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextIntent = new Intent(LoginActivity.this,
+                        MealPlanCell.class);
+                startActivity(nextIntent);
+            }
+        });
     }
 
     private void populateAutoComplete() {
--- app/src/main/java/com/pvi/jd/gt/personalvirtualinventories/Controller/SplashScreen.java
+++ app/src/main/java/com/pvi/jd/gt/personalvirtualinventories/Controller/SplashScreen.java
@@ -1,7 +1,10 @@
 package com.pvi.jd.gt.personalvirtualinventories.Controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.pvi.jd.gt.personalvirtualinventories.R;
 
@@ -11,5 +14,15 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Button next = (Button) findViewById(R.id.next_button_splash);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextIntent = new Intent(SplashScreen.this,
+                        QuestionOne.class);
+                startActivity(nextIntent);
+            }
+        });
     }
 }
