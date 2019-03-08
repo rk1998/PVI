d6834a085740be52fcbb1dfe30cb7d21278f8616
francineluo <francine_luo@yahoo.com>
2019-03-07 22:05:00 -0500

    edited recyclerviewadapter to look better and have selection functionality, made layout consistent among all questionnaire screens, added list to q2, q3, and q6

--- app/src/main/java/com/pvi/jd/gt/personalvirtualinventories/Controller/QuestionSix.java
+++ app/src/main/java/com/pvi/jd/gt/personalvirtualinventories/Controller/QuestionSix.java
@@ -1,9 +1,17 @@
 package com.pvi.jd.gt.personalvirtualinventories.Controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import com.pvi.jd.gt.personalvirtualinventories.R;
+
+import java.util.ArrayList;
 
 public class QuestionSix extends AppCompatActivity {
 
@@ -11,5 +19,32 @@ public class QuestionSix extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_six);
+
+        ArrayList<String> tools = new ArrayList<>();
+        tools.add("Instant Pot");
+        tools.add("Blender");
+        tools.add("Food Processor");
+        tools.add("Slow Cooker");
+        tools.add("Grater");
+        tools.add("Whisk");
+        tools.add("Vegetable Peeler");
+        tools.add("Baking Sheet Pan");
+        tools.add("NutriBullet");
+
+        RecyclerView toolList = (RecyclerView) findViewById(R.id.tool_list);
+        toolList.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        toolList.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, tools);
+        toolList.setAdapter(adapter);
+
+        Button next = (Button) findViewById(R.id.next_button_q6);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextIntent = new Intent(QuestionSix.this,
+                        MainScreen.class);
+                startActivity(nextIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/pvi/jd/gt/personalvirtualinventories/Controller/QuestionThree.java
+++ app/src/main/java/com/pvi/jd/gt/personalvirtualinventories/Controller/QuestionThree.java
@@ -3,10 +3,15 @@ package com.pvi.jd.gt.personalvirtualinventories.Controller;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
 import com.pvi.jd.gt.personalvirtualinventories.R;
+
+import java.util.ArrayList;
 
 public class QuestionThree extends AppCompatActivity {
 
@@ -14,6 +19,19 @@ public class QuestionThree extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_three);
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Gluten");
+        list.add("Dairy");
+        list.add("Egg");
+        list.add("Peanut");
+        list.add("Soy");
+
+        RecyclerView toolList = (RecyclerView) findViewById(R.id.allergy_list);
+        toolList.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        toolList.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, list);
+        toolList.setAdapter(adapter);
 
         Button next = (Button) findViewById(R.id.next_button_q3);
         next.setOnClickListener(new View.OnClickListener() {
