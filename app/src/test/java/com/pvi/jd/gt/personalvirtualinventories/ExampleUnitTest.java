b309fac78619f3d806746b7c16e2f87a6e3f3711
Paige Rutherford <paigerutherford8@gmail.com>
2019-03-10 15:33:10 -0400

    add calls for create user, check if email in user; add md5 hashing for passwords

--- app/src/test/java/com/pvi/jd/gt/personalvirtualinventories/ExampleUnitTest.java
+++ app/src/test/java/com/pvi/jd/gt/personalvirtualinventories/ExampleUnitTest.java
@@ -2,6 +2,11 @@ package com.pvi.jd.gt.personalvirtualinventories;
 
 import org.junit.Test;
 
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import static org.junit.Assert.*;
 
 /**
@@ -13,5 +18,19 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void checkMD5() {
+        try {
+            byte[] bytesOfMessage = "password".getBytes("UTF-8");
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] thedigest = md.digest(bytesOfMessage);
+            BigInteger bigInt = new BigInteger(1,thedigest);
+            String hashtext = bigInt.toString(16);
+            assertEquals("5f4dcc3b5aa765d61d8327deb882cf99", hashtext);
+        } catch (UnsupportedEncodingException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 }
