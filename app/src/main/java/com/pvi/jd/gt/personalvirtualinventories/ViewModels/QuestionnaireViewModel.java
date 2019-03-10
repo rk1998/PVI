08539322be61b2302449bb6e20fb76dc2b5ebd58
Paige Rutherford <paigerutherford8@gmail.com>
2019-03-10 17:44:36 -0400

    add support for user name

--- app/src/main/java/com/pvi/jd/gt/personalvirtualinventories/ViewModels/QuestionnaireViewModel.java
+++ app/src/main/java/com/pvi/jd/gt/personalvirtualinventories/ViewModels/QuestionnaireViewModel.java
@@ -24,8 +24,9 @@ public class QuestionnaireViewModel extends ViewModel {
      * @param email user's email
      * @param password user's password
      */
-    public void createNewUser(String email, String password) {
+    public void createNewUser(String name, String email, String password) {
         User newUser = new User(email, password);
+        newUser.setName(name);
         userRepo.setTempUser(newUser);
     }
 
