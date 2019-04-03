dd94c69125fa8dd8769bc1ea72fb47fef6a2c27e
Dixie <dixierich@lawn-143-215-127-140.lawn.gatech.edu>
2019-04-03 10:20:53 -0400

    updated grocery list layout

--- app/src/main/java/com/pvi/jd/gt/personalvirtualinventories/Controller/GroceryList.java
+++ app/src/main/java/com/pvi/jd/gt/personalvirtualinventories/Controller/GroceryList.java
@@ -1,17 +1,30 @@
 package com.pvi.jd.gt.personalvirtualinventories.Controller;
 
-import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.pvi.jd.gt.personalvirtualinventories.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class GroceryList extends AppCompatActivity {
     private RecyclerView recyclerView;
-    private RecyclerViewAdapter adapter;
+    private GroceryRecycler adapter;
     private RecyclerView.LayoutManager layoutManager;
 
     @Override
@@ -19,21 +32,22 @@ public class GroceryList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grocery_list);
 
-        recyclerView = (RecyclerView) findViewById(R.id.grocery_list);
-        recyclerView.setHasFixedSize(true);
 
-        // data to populate the RecyclerView with
         ArrayList<String> groceryItems = new ArrayList<>();
         groceryItems.add("16 oz. Mozzarella Cheese");
         groceryItems.add("18 oz. Tomato Sauce");
         groceryItems.add("12 Taco Shells");
         groceryItems.add("Frozen Meatballs");
 
-        // set up the RecyclerView
-        RecyclerView recyclerView = findViewById(R.id.grocery_list);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new RecyclerViewAdapter(this, groceryItems);
-        //adapter.setClickListener(this);
-        recyclerView.setAdapter(adapter);
+
+        RecyclerView toolList = (RecyclerView) findViewById(R.id.grocery_list);
+        toolList.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        toolList.setLayoutManager(new LinearLayoutManager(this));
+        final GroceryRecycler adapter = new GroceryRecycler(this, groceryItems);
+        toolList.setAdapter(adapter);
+
+
     }
+
+
 }
