f158c1d7f277b02f597201f72803bb576355ad7a
francineluo <francine_luo@yahoo.com>
2019-04-07 15:41:38 -0400

    added nav bar to inventory

--- app/src/main/java/com/pvi/jd/gt/personalvirtualinventories/Controller/Inventory.java
+++ app/src/main/java/com/pvi/jd/gt/personalvirtualinventories/Controller/Inventory.java
@@ -3,12 +3,19 @@ package com.pvi.jd.gt.personalvirtualinventories.Controller;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.pvi.jd.gt.personalvirtualinventories.Model.IngredientQuantity;
 import com.pvi.jd.gt.personalvirtualinventories.R;
@@ -16,18 +23,31 @@ import com.pvi.jd.gt.personalvirtualinventories.ViewModels.InventoryViewModel;
 
 import java.util.ArrayList;
 
-public class Inventory extends AppCompatActivity {
+public class Inventory extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private InventoryViewModel viewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_inventory);
+        setSupportActionBar(toolbar);
 
         getSupportActionBar().setTitle("INVENTORY");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        ArrayList<String> inventoryItems = new ArrayList<>();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_inventory);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view_inventory);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        getSupportActionBar().setTitle("INVENTORY");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final ArrayList<String> inventoryItems = new ArrayList<>();
         inventoryItems.add("food 1");
         inventoryItems.add("food 2");
         inventoryItems.add("food 3");
@@ -40,16 +60,41 @@ public class Inventory extends AppCompatActivity {
             @Override
             public void onChanged(@Nullable ArrayList<IngredientQuantity> ingredientQuantities) {
                 ArrayList<String> displayList = viewModel.getInventoryDisplay(ingredientQuantities);
-                RecyclerView toolList = (RecyclerView) findViewById(R.id.inventory_list);
-                toolList.addItemDecoration(new DividerItemDecoration(Inventory.this,
+                RecyclerView inventoryList = (RecyclerView) findViewById(R.id.inventory_list);
+                inventoryList.addItemDecoration(new DividerItemDecoration(Inventory.this,
                         DividerItemDecoration.VERTICAL));
-                toolList.setLayoutManager(new LinearLayoutManager(Inventory.this));
+                inventoryList.setLayoutManager(new LinearLayoutManager(Inventory.this));
                 final InventoryRecycler adapter = new InventoryRecycler(Inventory.this,
-                        displayList);
-                toolList.setAdapter(adapter);
+                        inventoryItems);
+                inventoryList.setAdapter(adapter);
             }
         });
+    }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
 
+        if (id == R.id.meal_planning) {
+            Intent intent = new Intent(this,
+                    MainScreen.class);
+            startActivity(intent);
+        } else if (id == R.id.inventory) {
+            //
+        } else if (id == R.id.grocery_list) {
+            Intent intent = new Intent(this,
+                    GroceryList.class);
+            startActivity(intent);
+        } else if (id == R.id.settings) {
+
+        }
+
+        item.setChecked(true);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_inventory);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
